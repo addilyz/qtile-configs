13f89fd54ad64a1091b57f445270d54ca4793dbc
Addilyn McAllister <109492195+addilys@users.noreply.github.com>
2023-06-05 14:13:07 -0600

    Create addilyslayout.sh

---
+++ .screenlayout/addilyslayout.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-1 --primary --mode 2560x1440 --pos 0x374 --rotate normal --output DP-2 --mode 1920x1080 --pos 3640x135 --rotate normal --output DP-3 --off --output HDMI-1 --mode 1920x1080 --pos 2560x0 --rotate right
